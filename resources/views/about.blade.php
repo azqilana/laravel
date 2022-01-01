d9fc060e11fae723be01bf0563d7bc30c24934fa
azqilana <mazqulunu@gmail.com>
2022-01-02 01:44:38 +0800

    tambah laravel

---
+++ resources/views/about.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Halaman About</title>
+</head>
+<body>
+    <h1>Halaman About</h1>
+    <img src="img/<?= $img ?>" alt="azqilan">
+    <div class="container">
+    <a>Nama :<?= $nama ?></a>
+    <a>Umur :<?= $umur ?></a>
+    <a>Emai :<?= $email ?></a>
+    </div>
+</body>
+</html>
